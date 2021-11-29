92e49e3b7af9740e6b503730f7087371a23b4e90
ibnat136 <ibnatnuha42@gmail.com>
2021-11-29 17:20:12 -0500

    Some minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const path = require ('path');
 
 const indexRouter = require('./routes/index.js')
 
 app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
+app.set('views',path.join(__dirname + '/views'));
 app.set('layout', 'layouts/layouts')
 app.use(expressLayouts)
 app.use(express.static('public'))

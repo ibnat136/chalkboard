c51fcb05a414bd8a7906a185305f5f0ad6d5b294
Miss Khatun <pritykha21@gmail.com>
2021-11-30 21:04:38 -0500

    adding

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,7 @@
 
-<<<<<<< HEAD
-
 if(process.env.NODE_ENV!=='production'){
-    require('dotenv').config()
+  require('dotenv').config()
 }
-=======
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-const path = require ('path');
->>>>>>> 5b1765e9faab021117cba5ff25e7b5945f078cb1
 
 const express= require('express')
 const app= express()
@@ -23,8 +15,8 @@ const methodOverride = require('method-override')
 
 const initializePassport= require('./passport-config')
 initializePassport(passport, 
-    userEmail=> users.find(user=> user.userEmail==userEmail),
-    id=>users.find(user=> user.id==id)
+  userEmail=> users.find(user=> user.userEmail==userEmail),
+  id=>users.find(user=> user.id==id)
 )
 
 const users=[]
@@ -33,99 +25,82 @@ app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
+secret: process.env.SESSION_SECRET,
+resave: false,
+saveUninitialized: false
 }))
 
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
-
 const { dirname } = require('path');
 const path = require('path');
 
-
-app.set('view engine', 'ejs')
-app.set('views',path.join(__dirname + '/views'));
-app.set('layout', 'layouts/layouts')
-app.use(expressLayouts)
-app.use(express.static('public'))
-
 app.use(express.static(path.join(__dirname,'./static')))
 
-<<<<<<< HEAD
 app.get('/',checkNotAuthenticated,(req,res)=>{
-=======
-
-app.get('/',(req,res)=>{
->>>>>>> 5b1765e9faab021117cba5ff25e7b5945f078cb1
-    res.render('page/index.ejs')
+  res.render('page/index.ejs')
 })
 
 app.get('/student',checkAuthenticated,(req,res)=>{
-    res.render('page/studentPage.ejs')
+  res.render('page/studentPage.ejs')
 })
 
 app.post('/', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/student',
-    failureRedirect: '/',
-    failureFlash: true
-  }))
-  
+  successRedirect: '/student',
+  failureRedirect: '/',
+  failureFlash: true
+}))
 
 
-  app.get('/signup',checkNotAuthenticated, (req,res)=>{
-    res.render('page/signup.ejs')
+
+app.get('/signup',checkNotAuthenticated, (req,res)=>{
+  res.render('page/signup.ejs')
 })
-<<<<<<< HEAD
 
 
 
 
 app.post('/signup',checkNotAuthenticated, async(req,res)=>{
-    try{
-        const hashedPassword=await bcrypt.hash(req.body.userPassword,10)
-        users.push({
-        id: Date.now().toString(),
-        userEmail: req.body.userEmail,
-        userPassword: hashedPassword,
-        school: req.body.school,
-        dob: req.body.dateToSend
+  try{
+      const hashedPassword=await bcrypt.hash(req.body.userPassword,10)
+      users.push({
+      id: Date.now().toString(),
+      userEmail: req.body.userEmail,
+      userPassword: hashedPassword,
+      school: req.body.school,
+      dob: req.body.dateToSend
 
-        })
-        res.redirect('/')
-        
-    }catch{
-        res.redirect('/signup')
+      })
+      res.redirect('/')
+      
+  }catch{
+      res.redirect('/signup')
 
-    }
-    console.log(users)
+  }
+  console.log(users)
 
 })
 
 app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/')
-  })
+  req.logOut()
+  res.redirect('/')
+})
 
 function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-  
-    res.redirect('/')
+  if (req.isAuthenticated()) {
+    return next()
   }
 
-  function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/student')
-    }
-    next()
+  res.redirect('/')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/student')
   }
+  next()
+}
 
 app.listen(3002)
-=======
-app.listen(3002);
->>>>>>> 5b1765e9faab021117cba5ff25e7b5945f078cb1
